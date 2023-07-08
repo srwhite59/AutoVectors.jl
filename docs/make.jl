106325254e1874ba4e4e5fcda019a923abdae7a1
Steven White <srwhite@uci.edu>
2023-07-08 10:30:22 -0700

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,21 @@
 using Documenter, AutoVectors
 
 makedocs(sitename="My Documentation")
+
+deploydocs(
+    root = "<current-directory>",
+    target = "build",
+    dirname = "",
+    repo = "<required>",
+    branch = "gh-pages",
+    deps = nothing | <Function>,
+    make = nothing | <Function>,
+    devbranch = nothing,
+    devurl = "dev",
+    versions = ["stable" => "v^", "v#.#", devurl => devurl],
+    forcepush = false,
+    deploy_config = auto_detect_deploy_system(),
+    push_preview = false,
+    repo_previews = "github.com/srwhite59/AutoVectors.jl.git",
+    branch_previews = "gh-pages",
+)
